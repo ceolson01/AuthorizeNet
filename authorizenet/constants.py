91c96f056b1678e8fdf9429dcca4dda8586ed5f1
curtis <curtis@bytepro.net>
2017-11-22 17:38:50 -0500

    Set default logging level to WARNING to stop logging attempt on every transaction

--- authorizenet/constants.py
+++ authorizenet/constants.py
@@ -8,9 +8,9 @@ import logging
 class constants(object):
     """All the constants are defined here
     Define all your constants instead of using magic numbers in the
-    code. 
+    code.
     """
-    
+
     '''Environments'''
     SANDBOX = 'https://apitest.authorize.net/xml/v1/request.api'
     PRODUCTION = 'https://api2.authorize.net/xml/v1/request.api'
@@ -21,10 +21,10 @@ class constants(object):
 
     '''xml encoding'''
     xml_encoding = 'utf-8'
-    
+
     '''xml headers'''
     headers = {'Content-Type' : 'application/xml', 'version' : '1.0', 'encoding' : xml_encoding}
-    
+
     """
     Following constants are defined and used in the ARBSubscriptionStatusController
     Used to remove the "Status" element, that has been deprecated
@@ -34,32 +34,31 @@ class constants(object):
     '''ARBGetSubscriptionStatus <Status> tag'''
     StatusStart = '<Status>'
     StatusEnd = '</Status>'
-    
+
     '''response encoding'''
     response_encoding = 'ISO-8859-1'
-    
+
     '''note section of subscription status response'''
     note = ' note="Status with a capital \'S\' is obsolete."'
-    
+
     '''ns namespace 1'''
     nsNamespace1 = b'ns1:'
-    
+
     '''ns namespace 2'''
     nsNamespace2 = b':ns1'
-    
+
     '''default log file name'''
     defaultLogFileName = "anetsdk.log"
-    
+
     '''default logging level'''
-    #defaultLoggingLevel = logging.WARNING
-    defaultLoggingLevel = logging.DEBUG
-    
+    defaultLoggingLevel = logging.WARNING
+
     '''default log format'''
     defaultlogformat = '%(asctime)s %(message)s'
-    
+
     propertiesloggingfilename = "loggingfilename"
-    
+
     propertiesexecutionlogginglevel = "executionlogginglevel"
 
-    
+
 '''eof'''
